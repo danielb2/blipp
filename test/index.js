eb77487ca1b9e41129316348f8a86415bc8896b5
Yulia Tenincheva <yulia.tenincheva@mentormate.com>
2017-11-13 19:42:21 +0200

    Linted tests

--- test/index.js
+++ test/index.js
@@ -1,10 +1,7 @@
 'use strict';
 
 // Load modules
-
 const Hapi = require('hapi');
-const Code = require('code');
-const Lab = require('lab');
 
 const Blipp = require('../lib/');
 const Pkg = require('../package.json');
@@ -13,10 +10,10 @@ const Pkg = require('../package.json');
 const { expect, it, describe } = exports.lab = require('lab').script();
 
 // only one connection; results are in alphabetical order
-var internals = {
-    validateFunc: async function (request, username, password, h) {
+const internals = {
+    validateFunc: function (request, username, password, h) {
 
-        return {isValid: true, credentials: null};
+        return { isValid: true, credentials: null };
     },
     result: [{
         routes: [
@@ -53,11 +50,11 @@ var internals = {
 
 internals.prepareServer = async function (options) {
 
-    var server = new Hapi.Server();
+    const server = new Hapi.Server();
 
     try {
         await server.register(require('hapi-auth-basic'));
-        
+
         if (options.authType === 'findme') {
             server.auth.strategy('findme', 'basic', { validate: internals.validateFunc });
         }
@@ -66,13 +63,15 @@ internals.prepareServer = async function (options) {
             server.auth.strategy('findme', 'basic', { validate: internals.validateFunc });
             server.auth.default('findme');
         }
-        
-    } catch(err) {
+
+    }
+    catch (err) {
         console.log('Error: Couldn\'t register hapi-auth-basic', err);
     }
 
-    var api = {
+    const api = {
         register: function (plugin, pluginOptions) {
+
             plugin.route({
                 method: 'GET',
                 path: '/api',
@@ -91,7 +90,7 @@ internals.prepareServer = async function (options) {
     api.name = 'an api plugin';
     api.version = '1.0.0';
 
-    var main = {
+    const main = {
         register: function (plugin, pluginOptions) {
 
             plugin.route({
@@ -122,7 +121,7 @@ internals.prepareServer = async function (options) {
             plugin.route({
                 method: 'POST',
                 path: '/apost/{foo}/comment/{another}',
-                 options: {
+                options: {
                     auth: options.authType ? 'findme' : null,
                     handler: function (request, h) {
 
@@ -160,12 +159,12 @@ internals.prepareServer = async function (options) {
             }
         }
     });
-    
+
     try {
         await server.register([
             { plugin: Blipp, options: options.blippOptions },
-            { plugin: main,  options: {}},
-            { plugin: api,   options: {}}
+            { plugin: main,  options: {} },
+            { plugin: api,   options: {} }
         ]);
 
         await server.start();
@@ -173,80 +172,89 @@ internals.prepareServer = async function (options) {
         expect(server).to.exist();
 
         return server;
-    } catch(err) {
+    }
+    catch (err) {
         expect(err).to.not.exist();
     }
 };
 
-describe('routes', function () {
+describe('routes', () => {
 
     it('print route information', async () => {
-        
-        var saved = console.log;
-        var out = '';
-        console.log = (str) => out += str;
 
-        const server = await internals.prepareServer(false);
-        
+        const saved = console.log;
+        let out = '';
+        console.log = (str) => {
+
+            out += str;
+        };
+
+        await internals.prepareServer(false);
+
         console.log = saved;
         expect(out).to.not.match(/none.*main index/);
         expect(out).to.match(/DELETE.*post/);
-        
+
     });
 
     it('gets route information', async () => {
-        let blippOptions = {
+
+        const blippOptions = {
             showAuth: false,
             showStart: false
         };
-        
-        const server = await internals.prepareServer({blippOptions});
-        
-        var info = server.plugins[Pkg.name].info();
+
+        const server = await internals.prepareServer({ blippOptions });
+
+        const info = server.plugins[Pkg.name].info();
         delete info[0].uri;
         expect(info).to.equal(internals.result);
-        var text = server.plugins[Pkg.name].text();
+        const text = server.plugins[Pkg.name].text();
         expect(text).to.not.match(/none.*main index/);
     });
 
     it('gets route information with auth', async () => {
-        let blippOptions = {
+
+        const blippOptions = {
             showAuth: true,
             showStart: false
         };
-        
+
         const server = await internals.prepareServer({ blippOptions, authType: 'findme' });
-        
-        var info = server.plugins[Pkg.name].info();
+
+        const info = server.plugins[Pkg.name].info();
         delete info[0].uri;
         expect(info).to.equal(internals.authResult);
 
-        var text = server.plugins[Pkg.name].text();
+        const text = server.plugins[Pkg.name].text();
         expect(text).to.match(/none.*main index/);
         expect(text).to.match(/findme.*api routes/);
         expect(text).to.match(/hi.*findme/);
     });
 
     it('gets route information with default', async () => {
-        let blippOptions = {
+
+        const blippOptions = {
             showAuth: true,
             showStart: false
         };
-        
+
         const server = await internals.prepareServer({ blippOptions, authType: 'default' });
 
-        var info = server.plugins[Pkg.name].info();
+        const info = server.plugins[Pkg.name].info();
         delete info[0].uri;
         expect(info).to.equal(internals.defaultAuthResult);
-        var text = server.plugins[Pkg.name].text();
+        const text = server.plugins[Pkg.name].text();
         expect(text).to.match(/none.*main index/);
         expect(text).to.match(/findme.*api routes/);
     });
 
     it('fails with invalid options', async () => {
+
         try {
-            await internals.prepareServer({ blippOptions: { derp: true } })
-        } catch(err) {
+            await internals.prepareServer({ blippOptions: { derp: true } });
+        }
+        catch (err) {
             expect(err).to.exist();
         };
     });
